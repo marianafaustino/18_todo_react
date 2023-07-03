45647c0127ea01eb450307a0abbd61b3e6b2cd9a
Mariana Faustino <marifaustino@gmail.com>
2023-07-03 16:15:58 -0300

    Finalizando lista

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,8 +13,16 @@ const TaskList = ({taskList}: Props) => {
     <>
         {taskList.length > 0 ? (
             taskList.map((task)=>(
-                <div key={task.id}>
-                    <p>{task.title}</p>
+                <div key={task.id} className={styles.task}>
+                    <div className={styles.details}>
+                        <h4>{task.title}</h4>
+                        <p>Dificuldade: {task.difficulty}</p>
+                    </div>
+
+                    <div className={styles.actions}>
+                        <i className='bi bi-pencil'></i>
+                        <i className='bi bi-trash'></i>
+                    </div>
                 </div>
             ))
         ): (
