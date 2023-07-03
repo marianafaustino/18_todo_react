57e0be387acde9d38666190c6a148ca82d0896e2
Mariana Faustino <marifaustino@gmail.com>
2023-07-03 17:25:11 -0300

    Função de remover tarefa da lista

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,15 @@ import TaskList from './components/TaskList';
 function App() {
 
   const [taskList, setTaskList] = useState<ITask[]>([])
+
+  const deleteTask = (id: number)=>{
+    setTaskList(
+      taskList.filter((task)=>{
+        return task.id !== id
+      })
+    )
+  }
+
   return (
     <div>
       <Header/>
@@ -26,7 +35,7 @@ function App() {
 
         <div>
           <h2>Suas tarefas:</h2>
-          <TaskList taskList={taskList}/>
+          <TaskList taskList={taskList} handleDelete={deleteTask}/>
         </div>
       </main>
 
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -6,9 +6,10 @@ import { ITask } from '../interfaces/Task'
 
 type Props = {
     taskList: ITask[]
+    handleDelete(id: number): void
 }
 
-const TaskList = ({taskList}: Props) => {
+const TaskList = ({taskList, handleDelete}: Props) => {
   return (
     <>
         {taskList.length > 0 ? (
@@ -21,7 +22,7 @@ const TaskList = ({taskList}: Props) => {
 
                     <div className={styles.actions}>
                         <i className='bi bi-pencil'></i>
-                        <i className='bi bi-trash'></i>
+                        <i className='bi bi-trash' onClick={()=> handleDelete(task.id)}></i>
                     </div>
                 </div>
             ))
